f73a185b0ea157a783f6b7e5b8521a542cc92df4
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2024-01-31 17:26:01 +0100

    starter code

---
+++ script.js
@@ -0,0 +1,15 @@
+// *** Global Variables ***
+const gameBox = document.querySelector("#game-box");
+const ball = document.querySelector("#ball");
+const paddle = document.querySelector("#paddle");
+
+
+// *** Game Functions ***
+
+
+
+// *** Game Loop Interval ***
+
+
+
+// *** Event Listeners ***
