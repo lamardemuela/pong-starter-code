f87f659f896b5b4bca76ca4ffca88222e2783764
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2024-04-17 21:34:08 +0200

    added code for creating ball and paddle

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 // *** Global Variables ***
 const gameBoxNode = document.querySelector("#game-box");
-const ballNode = document.querySelector("#ball");
-const paddleNode = document.querySelector("#paddle");
+
+// const ballNode = document.createElement("div"); // se crea la pelotita
+// ballNode.id = "ball"; // se asigna un id a la pelotita (para CSS)
+// gameBoxNode.append(ballNode); // se añade la pelotita a la caja de juego
+
+// const paddleNode = document.createElement("div"); // se crea la paleta
+// paddleNode.id = "paddle"; // se asigna un id a la paleta (para CSS)
+// gameBoxNode.append(paddleNode); // se añade la pelotita a la caja de juego
+
+
 
 
 // *** Game Functions ***
+
+
 
 
 
@@ -12,4 +22,10 @@ const paddleNode = document.querySelector("#paddle");
 
 
 
+
+
 // *** Event Listeners ***
+
+
+
+
